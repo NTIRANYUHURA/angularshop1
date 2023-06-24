b7c81e8cfc71b26c5a240b19780b615e67e9adeb
NTIRANYUHURA <floribertnt@gmail.com>
2023-06-24 08:34:02 +0200

    implementation component login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
-import { FormsModule } from '@angular/forms';
+import { CartComponent } from './component/cart/cart.component';
 import { ProductsComponent } from './component/products/products.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FilterPipe } from './shared/filter.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddProductComponent } from './shared/component/add-product/add-product.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    CartComponent,
     ProductsComponent,
-
+    FilterPipe,
+    AddProductComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
